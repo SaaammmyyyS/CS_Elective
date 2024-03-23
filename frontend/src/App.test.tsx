979ede707fa12d37bb93608a3442e124338fa4ca
Ivan <ivansamwabina@gmail.com>
2024-03-23 18:29:28 +0800

    feat: added Login Page and fix frontend folder

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Register from './Register';
+import Register from './pages/Register';
 
 test('renders learn react link', () => {
   render(<Register />);
---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import {
+    BrowserRouter,
+    Route,
+    Routes
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/login";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
