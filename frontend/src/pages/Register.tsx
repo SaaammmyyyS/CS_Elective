e2f78243c35f0e8381a60f5632f42cfe43ee840c
Ivan <ivansamwabina@gmail.com>
2024-03-28 12:56:53 +0800

    chore: replaced input btn into TextField component

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,6 @@
 import React, {SyntheticEvent, useState} from 'react';
 import Navbar from '../components/navbar';
+import TextField from '../components/ui/TextField';
 
 const Register = () => {
   const [name, setName] = useState('');
@@ -35,30 +36,45 @@ const Register = () => {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
         </div>
-
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" onSubmit={submit}>
             <div>
-              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
               <div className="mt-2">
-                <input id="name" name="name" onChange={e => setName(e.target.value)} autoComplete="name" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                <TextField
+                  id="name"
+                  label="Name"
+                  type="text"
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+                  name="email"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="mt-2">
+                <TextField
+                  id="email"
+                  label="Email"
+                  type="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  name="email"
+                />
               </div>
             </div>
 
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
               <div className="mt-2">
-                <input id="email" name="email" type="email" onChange={e => setEmail(e.target.value)} autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                <TextField
+                  id="password"
+                  label="Password"
+                  type="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  name="password"
+                />
               </div>
             </div>
-
-            <div>
-              <div className="mt-2">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                <input id="password" name="password" onChange={e => setPassword(e.target.value)} type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-              </div>
-            </div>
-
             <div>
               <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
             </div>
