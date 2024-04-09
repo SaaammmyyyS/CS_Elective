d8bd1780c84d93e4e637fc7421a1b9f4442a8526
Ivan <ivansamwabina@gmail.com>
2024-04-09 19:00:13 +0800

    add model and api for scraping jobstreet

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from CS_Elective import settings
+
 class User(AbstractUser):
     ROLE_CHOICES = (
         ('admin', 'Admin'),
@@ -16,3 +17,18 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+class JobListing(models.Model):
+    STATUS_CHOICES = (
+        ('open', 'Open'),
+        ('applied', 'Applied'),
+        ('archived', 'Archived'),
+    )
+
+    title = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    url = models.CharField(max_length=255, default='')
+    # description = models.TextField()
+    # date_posted = models.DateField()
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='open')
