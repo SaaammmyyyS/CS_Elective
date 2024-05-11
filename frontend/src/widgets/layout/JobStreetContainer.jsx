fc0cc4985d2393e30778f3de95daa5d13d29598e
RonBersabal <rjlbersabal@addu.edu.ph>
2024-05-11 22:27:24 +0800

    feat(job listings): Update pagination implementation for compatibility with PrimeReact - Refactor pagination logic to ensure compatibility with PrimeReact DataTable component - Removed pagination classes on views.py

--- frontend/src/widgets/layout/JobStreetContainer.jsx
+++ frontend/src/widgets/layout/JobStreetContainer.jsx
@@ -1,29 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { Typography, Input } from "@material-tailwind/react";
-import Pagination from "../ui/pagination";
 import NavigateNextRoundedIcon from "@mui/icons-material/NavigateNextRounded";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { FilterMatchMode } from "primereact/api";
+import { Button } from "@material-tailwind/react";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+} from "@heroicons/react/24/solid";
 
 const JobStreetContainer = ({ refreshKey }) => {
   const [jobListings, setJobListings] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const pageSize = 10;
-
   const [filters, setFilters] = useState({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS },
   });
 
   const baseUrl = `http://localhost:8000/api/job-listings`;
   useEffect(() => {
-    fetchData(currentPage);
-  }, [currentPage, refreshKey]);
+    fetchData();
+  }, [refreshKey]);
 
-  const fetchData = async (page) => {
+  const fetchData = async () => {
     try {
-      const response = await fetch(baseUrl + `?page=${page}`, {
+      const response = await fetch(baseUrl, {
         method: "GET",
         credentials: "include",
       });
@@ -33,14 +35,89 @@ const JobStreetContainer = ({ refreshKey }) => {
       }
 
       const responseData = await response.json();
-      const { results, count } = responseData;
-
-      setJobListings(results);
-      setTotalPages(Math.ceil(count / pageSize));
-      setCurrentPage(page);
+      setJobListings(responseData);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
+  };
+
+  const template = {
+    layout: "FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink",
+    FirstPageLink: (options) => {
+      return (
+        <Button
+          variant="text"
+          className="flex w-4 justify-center bg-transparent text-black hover:bg-gray-200"
+          onClick={options.onClick}
+          disabled={options.disabled}
+        >
+          <span className="">
+            <ChevronDoubleLeftIcon className="h-4 w-4" />
+          </span>
+        </Button>
+      );
+    },
+    LastPageLink: (options) => {
+      return (
+        <Button
+          variant="text"
+          className="flex w-4 justify-center bg-transparent text-black hover:bg-gray-200"
+          onClick={options.onClick}
+          disabled={options.disabled}
+        >
+          <span className="">
+            <ChevronDoubleRightIcon className="h-4 w-4" />
+          </span>
+        </Button>
+      );
+    },
+    PrevPageLink: (options) => {
+      return (
+        <Button
+          variant="text"
+          size="md"
+          className="mr-2 bg-transparent text-black hover:bg-gray-200"
+          onClick={options.onClick}
+          disabled={options.disabled}
+        >
+          <span className="flex space-x-4">
+            <ChevronLeftIcon className="mr-2 h-4 w-4" />
+            Previous
+          </span>
+        </Button>
+      );
+    },
+    NextPageLink: (options) => {
+      return (
+        <Button
+          variant="text"
+          size="md"
+          className="ml-2 bg-transparent text-black hover:bg-gray-200"
+          onClick={options.onClick}
+          disabled={options.disabled}
+        >
+          <span className="flex">
+            Next
+            <ChevronRightIcon className="ml-2 h-4 w-4" />
+          </span>
+        </Button>
+      );
+    },
+    PageLinks: (options) => {
+      const isActive = options.page === options.currentPage;
+      const buttonClassName = isActive ? "bg-gray-100 " : "";
+
+      return (
+        <Button
+          type="button"
+          variant="text"
+          className={buttonClassName}
+          onClick={options.onClick}
+        >
+          {options.page + 1}
+        </Button>
+      );
+    },
   };
 
   const renderVisitLink = (rowData) => {
@@ -80,9 +157,15 @@ const JobStreetContainer = ({ refreshKey }) => {
       <div className="w-full min-w-[640px]">
         <DataTable
           value={jobListings}
-          className="my-4 table-auto border-collapse space-x-2 whitespace-nowrap border-black px-8"
+          className="my-4 table-auto border-collapse space-x-2 overflow-y-scroll whitespace-nowrap border-black px-8"
           filters={filters}
           sortMode="multiple"
+          paginator
+          rows={10}
+          paginatorTemplate={template}
+          paginatorLeft
+          paginatorClassName="mt-5 align-center"
+          totalRecords={120}
         >
           <Column
             field="title"
@@ -113,16 +196,6 @@ const JobStreetContainer = ({ refreshKey }) => {
             headerClassName="uppercase text-blue-gray-400 font-bold text-sm space-x-2"
           />
         </DataTable>
-      </div>
-
-      <div className="flex w-full justify-center p-2 md:justify-end">
-        <Pagination
-          className="p-5"
-          baseUrl={baseUrl}
-          currentPage={currentPage}
-          totalPages={totalPages}
-          fetchData={fetchData}
-        />
       </div>
     </div>
   );
